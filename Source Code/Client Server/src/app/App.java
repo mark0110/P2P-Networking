7d46d1d705b8183e92a5d9b4518168d4549e56d1
Mike <m3teitel@ryerson.ca>
2019-03-19 14:51:31 -0400

    Adding client server stuff

--- Source Code/Client Server/src/app/App.java
+++ Source Code/Client Server/src/app/App.java
@@ -1,15 +1,42 @@
 package app;
+import java.io.File;
+import java.io.IOException;
+import java.net.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
-    }
-    public int hashValue(String name){
+    public static void main(String[] args) {
+        getOtherIP("");
+        try {
+            File folder = new File("./src/app/Photos");
+            File[] listOfFiles = folder.listFiles();
+            for (int i = 0; i < listOfFiles.length; i++) {
+                if (listOfFiles[i].isFile()) {
+                    System.out.println("File " + listOfFiles[i].getName() + ":" + hashValue(listOfFiles[i].getName()));
+                }
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+}
+    private static int hashValue(String name){
         int hashValue = 0;
         for(int i = 0; i < name.length(); i++){
             hashValue += (int) name.charAt(i);
         }
         hashValue = hashValue % 4;
         return hashValue;
+    }
+    private static String[] getOtherIP(String ip){
+        String test = "Hello!!";
+        byte[] buf = test.getBytes();
+        try {
+            DatagramSocket datagramSocket = new DatagramSocket();
+            InetAddress localIp = InetAddress.getLocalHost();
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, localIp, 20101);
+            datagramSocket.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
---
+++ Source Code/Client Server/src/app/udptestserver.java
@@ -0,0 +1,52 @@
+package app;
+
+// Java program to illustrate Server side
+// Implementation using DatagramSocket
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class udptestserver
+{
+    public static void main(String[] args) throws IOException
+    {
+        // Step 1 : Create a socket to listen at port 1234
+        DatagramSocket ds = new DatagramSocket(20101);
+        byte[] receive = new byte[65535];
+
+        DatagramPacket DpReceive = null;
+        while (true)
+        {
+
+            // Step 2 : create a DatgramPacket to receive the data.
+            DpReceive = new DatagramPacket(receive, receive.length);
+
+            // Step 3 : revieve the data in byte buffer.
+            ds.receive(DpReceive);
+
+            System.out.println("Client:-" + data(receive));
+
+            // Exit the server if the client sends "bye"
+
+                break;
+        }
+    }
+
+    // A utility method to convert the byte array
+    // data into a string representation.
+    public static StringBuilder data(byte[] a)
+    {
+        if (a == null)
+            return null;
+        StringBuilder ret = new StringBuilder();
+        int i = 0;
+        while (a[i] != 0)
+        {
+            ret.append((char) a[i]);
+            i++;
+        }
+        return ret;
+    }
+}
