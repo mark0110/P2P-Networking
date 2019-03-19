61cdaf67d226fd7c4af0c626796e96867548d026
Mike <m3teitel@ryerson.ca>
2019-03-19 13:23:04 -0400

    added hash function

--- Source Code/Client Server/src/app/App.java
+++ Source Code/Client Server/src/app/App.java
@@ -4,4 +4,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
     }
+    public int hashValue(String name){
+        int hashValue = 0;
+        for(int i = 0; i < name.length(); i++){
+            hashValue += (int) name.charAt(i);
+        }
+        hashValue = hashValue % 4;
+        return hashValue;
+    }
 }
