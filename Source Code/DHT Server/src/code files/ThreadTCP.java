835c0b5988ecfdce402f7a3b081d27fe3999dfa4
Mark <mark.volfson@gmail.com>
2019-04-09 16:04:01 -0400

    updated

--- Source Code/DHT Server/src/code files/ThreadTCP.java
+++ Source Code/DHT Server/src/code files/ThreadTCP.java
@@ -3,18 +3,18 @@ import java.util.Set;
 
 //the DHT servers use 2 threads, 1 more the TCP connection and the other for the UDP connection
 //this thread is responsible for accepting TCP String packets and performing the tasks based on what is received
-public class ThreadTCP extends Thread{
+public class ThreadTCP extends Thread {
 
     //initializing
     String msg;
     ServerTCP server;
 
     //overriding the run method of the Thread class to perform the tasks that my program needs to
-    public void run(){
+    public void run() {
         while (true) {
             server = new ServerTCP();
             msg = server.getMsg();
-            System.out.println("TCP received: "+ msg);
+            System.out.println("TCP received: " + msg);
             msgRec(msg);
             server = null;
             System.gc();
@@ -32,23 +32,23 @@ public class ThreadTCP extends Thread{
         Iterator<String> it;
         switch (Integer.parseInt(arr[0])) {
             case 1:
-                if(msg.contains(DHTServer.server.ip)) {
+                if (msg.contains(DHTServer.server.ip)) {
                     udp = new ClientUDP(msg);
-                    System.out.println("Table after init: "+ DHTServer.server.h.toString());
+                    System.out.println("Table after init: " + DHTServer.server.h.toString());
                     break;
                 }
-                msg = msg +";"+ DHTServer.server.id + ";" + DHTServer.server.ip;
+                msg = msg + ";" + DHTServer.server.id + ";" + DHTServer.server.ip;
                 tcp = new ClientTCP(DHTServer.server.ipSuc, msg);
                 break;
             case 4:
-                if(msg.contains(DHTServer.server.ip)) {
-                    System.out.println("Table after exit: "+ DHTServer.server.h.toString());
+                if (msg.contains(DHTServer.server.ip)) {
+                    System.out.println("Table after exit: " + DHTServer.server.h.toString());
                     break;
                 }
                 if (DHTServer.server.h.containsValue(arr[1])) {
                     keys = DHTServer.server.h.keySet();
                     it = keys.iterator();
-                    while(it.hasNext()) {
+                    while (it.hasNext()) {
                         temp = it.next();
                         if (DHTServer.server.h.get(temp).equals(arr[1])) {
                             it.remove();
@@ -62,5 +62,4 @@ public class ThreadTCP extends Thread{
                 break;
         }
     }
-
 }
